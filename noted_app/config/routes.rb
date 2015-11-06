53ae271f2d049edcfc191fb73491201f85fbeb0f
David Malary <davidmalary@gmail.com>
2015-11-06 07:31:46 -0500

    readme updates

--- noted_app/config/routes.rb
+++ noted_app/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  # remove script after :users to view users/sign_up
 
   root 'users#index'
 
